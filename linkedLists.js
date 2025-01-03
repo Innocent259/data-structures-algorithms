bb3cbe2ce6a05d620230e3041ac1bfece67a4349
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2025-01-03 14:19:53 +0200

    Create LinkedList and basic methods

--- linkedLists.js
+++ linkedLists.js
@@ -6,3 +6,18 @@ class Node {
         this.next = null
     }
 }
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    isEmpty() {
+        return this.size === 0
+    } 
+
+    getSize() {
+        return this.size
+    }
+}
