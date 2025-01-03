a02cbe75c633c3c7ed943e84048f740968b632db
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2025-01-03 17:59:01 +0200

    Add prepend method

--- linkedLists.js
+++ linkedLists.js
@@ -1,23 +1,38 @@
 // Creating Node
 
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null
-        this.size = 0
-    }
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-    isEmpty() {
-        return this.size === 0
-    } 
+  isEmpty() {
+    return this.size === 0;
+  }
 
-    getSize() {
-        return this.size
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if(this.isEmpty()) {
+        this.head = node;
+    } else {
+        node.next = this.head;
+        this.head = node;
     }
+    this.size++;
+  } 
 }
+
+const list = new LinkedList();
+console.log("isEmpty", list.isEmpty());
+console.log("size", list.getSize());
